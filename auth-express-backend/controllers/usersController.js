c2fd3dfb6f6e1bed3840db692046503dd16fa04d
Gigi <gigi.scarborough@get-carrot.com>
2023-12-18 14:05:54 -0500

    add backend skeleton

--- auth-express-backend/controllers/usersController.js
+++ auth-express-backend/controllers/usersController.js
@@ -1,7 +1,6 @@
 const express = require("express");
-const bcrypt = require("bcryptjs");
-const jwt = require("jsonwebtoken");
-const passport = require("passport");
+// const bcrypt = require("bcryptjs");
+
 const {
   getOneUser,
   getOneUserByEmail,
@@ -11,64 +10,14 @@ const PRIVATE_KEY = process.env.PRIVATE_KEY;
 
 const users = express.Router();
 
+// LOGIN ROUTE
 users.post("/login", async (req, res) => {
-  const oneUser = await getOneUserByEmail(req.body);
-  if (oneUser) {
-    bcrypt.compare(req.body.password, oneUser.password).then((isMatch) => {
-      if (isMatch) {
-        const { id, email, firstname, lastname } = oneUser
-        const payload = { id, email, firstname, lastname };
-        res.json({success:true, user:payload })
-        // jwt.sign(
-        //   payload,
-        //   PRIVATE_KEY,
-        //   { expiresIn: 3600,
-        //    },
-        //   (err, token) => {
-        //     res.json({
-        //       success: true,
-        //       token: "Bearer " + token,
-        //       user:payload
-        //     });
-        //   }
-        // );
-      } else {
-        res.status(400).json({password: 'Incorrect password'});
-      }
-    });
-  } else {
-    res.status(404).json({ error: "not found!" });
-  }
+
 });
 
+// SIGN UP ROUTE
 users.post("/", async (req, res) => {
-  const registeredUser = await getOneUserByEmail(req.body);
-  if (registeredUser) {
-    return res
-      .status(400)
-      .json({ email: "A user has already registered with this address" });
-  } else {
-    const newUser = req.body;
-
-    bcrypt.genSalt(10, async (err, salt) => {
-      bcrypt.hash(newUser.password, salt, async (err, hash) => {
-        if (err) throw err;
-        console.log(hash, "   hash ");
-        newUser.password = hash;
-        try {
-          const createdUser = await createUser(newUser);
-          console.log(createdUser, " created user aqui");
-          if (createdUser.id) {
-            res.status(200).json(newUser);
-          }
-        } catch (error) {
-          res
-            .status(400)
-            .json({ error: "OH noeeees! You got a big huge error" });
-        }
-      });
-    });
-  }
+  
 });
 
 
