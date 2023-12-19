bffd5ebc31e4adeac5e875d2dbe1a564b2f37fbc
Anthony Huarneck <anthonyhuarneck@anthonys-air.myfiosgateway.com>
2023-12-18 20:19:39 -0500

    Edits schema.sql.

--- auth-express-backend/db/schema.sql
+++ auth-express-backend/db/schema.sql
@@ -8,9 +8,18 @@ CREATE DATABASE auth_dev;
 \c auth_dev;
 
 CREATE TABLE users (
-    id SERIAL PRIMARY KEY,
-    firstname TEXT NOT NULL,
-    lastname TEXT  NOT NULL,
-    email TEXT NOT NULL,
-    password TEXT  NOT NULL
+  user_id INT PRIMARY KEY AUTO_INCREMENT,
+  username VARCHAR(50) NOT NULL,
+  email VARCHAR(100) NOT NULL,
+  password_hash VARCHAR(255) NOT NULL,
+  registration_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE posts (
+  post_id INT PRIMARY KEY AUTO_INCREMENT,
+  user_id INT,
+  title VARCHAR(100) NOT NULL,
+  content TEXT,
+  creation_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  FOREIGN KEY (user_id) REFERENCES Users(user_id)
 );
